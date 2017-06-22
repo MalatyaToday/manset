a53455aa39a7e008229618063f36e14282541ccf
Murat Aksaç <aksac.murat@gmail.com>
2017-06-22 14:32:21 +0300

    Update head.php

--- head.php
+++ head.php
@@ -1,2 +1,2 @@
-<script src='http://code.jquery.com/jquery-1.4.2.min.js' type='text/javascript'></script>
-<script src='http://meftunmede.github.io/javascripts/JQuery_Haber_Manset_Slider.js' type='text/javascript'></script>
+<script src='../js/jquery-1.4.2.min.js' type='text/javascript'></script>
+<script src='../js/JQuery_Haber_Manset_Slider.js' type='text/javascript'></script>
